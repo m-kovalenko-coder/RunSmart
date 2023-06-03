8c2d77d3ae52bbf2830309f09756ff44ce9ffc2f
Maksym Kovalenko <m.kovalenko.coder@gmail.com>
2023-06-03 11:50:16 +0300

    First Commit Pulse

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg" alt="<"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/left.svg" alt=">"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+                    dots: true
+                }
+            }
+        ]
+    });
+});
